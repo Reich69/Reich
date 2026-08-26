344ddb68390c89e0261ec7ac4233e4292a7be639
Reich <rokonuu11230@gmail.com>
2026-08-26 23:24:31 +0600

    Initialize Supabase client with URL and key

---
+++ supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://vpfbhqyqandzsrcurwjj.supabase.co";
+const SUPABASE_KEY = "sb_publishable_FkR0OOScbwtuqMVp2uW_Fw_Ghge8i1K";
+
+const supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
